a4d01e230f2bb196b84d6c79079c5d69e89613ef
Marcus <bayareaviking@hotmail.com>
2022-11-21 20:40:49 -0800

    Merge pull request #1 from bayareaviking:marcus-testing More changes, more bugs

--- project.ps1
+++ project.ps1
@@ -6,3 +6,12 @@ Write-Warning -Message "This is all very silly"
 
 # We are looking for more stuff to write
 Get-Service
+
+if (Test-Connection 1.1.1.1) {
+    # Successful
+    Write-Output "It works!"
+} 
+else {
+    # Failure
+    Write-Warning -Message "It failed!"
+}
