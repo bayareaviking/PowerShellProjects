04c906de3fba3b17e6f37795ece3c26f1f41d549
Marcus <bayareaviking@hotmail.com>
2023-03-16 16:39:09 -0700

    Finished working with CSV and XML files.

---
+++ working_with_data.ps1
@@ -0,0 +1,27 @@
+# Working with Data
+
+## Exporting, Importing, and Converting Data
+### CSV files
+<# $csvData = Get-Process -Name "w*" | Select-Object -Property Name, Handles, Company | ConvertTo-Csv
+$csvData | Get-Member
+
+$csvData | ConvertFrom-Csv | Get-Member #>
+
+### XML Files
+<# Get-Process a* | Export-Clixml -Path ".\process.xml"-Force 
+Invoke-Item ".\process.xml"
+$xmlData = Import-Clixml ".\process.xml"
+
+$xmlData | Get-Member
+
+$xmlData | Select-Object -Property Name,WorkingSet #>
+
+# $convertedXml = Get-Process -Name "a*" | ConvertTo-Xml -As Document
+# $convertedXml.Save(".\convertedXml.xml")
+# Invoke-Item ".\convertedXml.xml"
+# #Import-Clixml ".\convertedXml.xml"
+
+# [xml]$xmlObject = Get-Content ".\convertedXml.xml"
+# $xmlObject
+
+### JSON files
